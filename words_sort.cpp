db7ba9e98e2b016b132a63c602d10f4f54c56405
Jinho Kim <55690757+Windachel@users.noreply.github.com>
2020-04-19 00:01:13 +0900

    Add files via upload

--- words_sort.cpp
+++ words_sort.cpp
@@ -5,16 +5,36 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	vector<string> arr;
-	string t;
+	//vector<string> arr;
+	string * arr = new string[n];
 	
 	for (int i = 0; i < n; i++) {
-		cin >> t;
-		arr.push_back(t);
+		cin >> arr[i];
 	}
 
-	sort(arr.begin(), arr.end());
-	for (auto i : arr)
-		cout << i << " ";
-	
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (arr[i].length() > arr[j].length()) {
+					auto temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (arr[i].length() == arr[j].length() && arr[i] > arr[j]) {
+				auto temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+
+
+	for (int i = 0; i < n; i++) {
+		if (arr[i] != arr[i + 1])
+			cout << arr[i] << " ";
+	}
 }
